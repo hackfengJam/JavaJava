2cceff65a1b1163f845b591692a009eea79bad52
HackFun <hackfeng624@gmail.com>
2019-03-01 02:06:46 +0800

    add synchroized

---
+++ MavenProj/concurrencydemo/src/main/java/com/hackfun/jiang/base/App.java
@@ -0,0 +1,13 @@
+package com.hackfun.jiang.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
